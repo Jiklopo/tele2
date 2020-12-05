3f9a3fc0ac2f7102289708585c12cd175c699d89
Timur Khaimovich <itima.com@gmail.com>
2020-12-05 13:00:44 +0600

    urls

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,18 @@
 from main.views import *
-
+from django.urls import path
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
 
+    path('login', UserLoginView.as_view(), name='login'),
+    path('logout', UserLogoutView.as_view(), name='logout'),
+    path('register', UserRegisterView.as_view(), name='register'),
+    path('change_password', UserChangePasswordView.as_view(), name='change_password'),
+
+    path('bookables/create', CreateBookableView.as_view(), name='bookable-create'),
+    path('bookables/<int:pk>/update', UpdateBookableView.as_view(), name='bookable-update'),
+    path('bookables/<int:pk>/delete', DeleteBookableView.as_view(), name='bookable-delete'),
+
+    path('bookings/create', CreateBookingView.as_view(), name='booking-create'),
+    path('bookings/<int:pk>/update', UpdateBookingView.as_view(), name='booking-update'),
+    path('bookings/<int:pk>/delete', DeleteBookingView.as_view(), name='booking-delete'),
 ]
